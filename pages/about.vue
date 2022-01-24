092d592a4a51bad01be5d641abf421b5ed0fdeef
Ogwo Chinaza <ogwo.chinaza@yahoo.com>
2022-01-24 04:41:15 +0100

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>About Dad Jokes</h1>
+    <p>This is an app that displays corny dad jokes</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About the app",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Welcome to the best corny dad jokes</h1>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Welcome to Dad Jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes",
+        },
+      ],
+    };
+  },
+};
+</script>
